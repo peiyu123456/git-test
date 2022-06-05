d0242867931445c64baaa07f6f47ecb6d22b0f2d
peiyu <peiyu0626.com>
2022-06-05 10:18:56 +0800

    hot-fix 1.0

--- src/main/java/com/tit/Hello.java
+++ src/main/java/com/tit/Hello.java
@@ -7,5 +7,8 @@ package com.tit;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("莫忘少年凌云志，曾许人间第一流！");
+        System.out.println("莫忘少年凌云志，曾许人间第一流！！");
+        System.out.println("莫忘少年凌云志，曾许人间第一流！！!");
+        System.out.println("莫忘少年凌云志，曾许人间第一流！！！！");
     }
 }
