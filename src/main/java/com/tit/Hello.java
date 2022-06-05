0db848f9f330b5d1b047842975c9f7da886f93be
peiyu <peiyu0626.com>
2022-06-05 10:12:43 +0800

    1.0

---
+++ src/main/java/com/tit/Hello.java
@@ -0,0 +1,11 @@
+package com.tit;
+
+/**
+ * @author peiyu
+ * @version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("莫忘少年凌云志，曾许人间第一流！");
+    }
+}
